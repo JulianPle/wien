7539d0ed9fd3560e4cf4a5b2bafac5a42a833e83
JulianPle <julian.plepla@gmx.de>
2023-04-23 15:01:31 +0200

    Layer mit popups versehen

---
+++ main.js
@@ -0,0 +1,108 @@
+/* Vienna Sightseeing Beispiel */
+
+// Stephansdom Objekt
+let stephansdom = {
+    lat: 48.208493,
+    lng: 16.373118,
+    title: "Stephansdom"
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([
+    stephansdom.lat, stephansdom.lng
+], 12);
+//thematische Layer
+let themaLayer = {
+    stops: L.featureGroup(),
+    lines: L.featureGroup(),
+    zones: L.featureGroup(),
+    sights: L.featureGroup().addTo(map)
+}
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "BasemapAT Grau": L.tileLayer.provider("BasemapAT.grau").addTo(map),
+    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
+    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay")
+}, {
+    "Vienna Sightseeing Haltestellen": themaLayer.stops,
+    "Vienna Sightseeing Linien": themaLayer.lines,
+    "Fußgängerzonen": themaLayer.zones,
+    "Sehenswürdigkeiten": themaLayer.sights
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+//Vienna Sightseeing: Haltestellen, Sights, Linien, Fußgängerzonen
+async function showStops(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    L.geoJSON(jsondata, {
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            layer.bindPopup(`
+            <h4><i class="fa-solid fa-bus"></i>&nbsp;${prop.LINE_NAME}</h4>
+            ${prop.STAT_NAME}
+            `);
+            //console.log(feature.properties);
+        }
+    }).addTo(themaLayer.stops);
+}
+async function showLines(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    L.geoJSON(jsondata, {
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            layer.bindPopup(`
+            <h4><i class="fa-solid fa-bus"></i>&nbsp;${prop.LINE_NAME}</h4>
+            <i class="fa-regular fa-circle-stop"></i>&nbsp;${prop.FROM_NAME}<br>
+            <i class="fa-solid fa-arrow-down"></i><br>
+            <i class="fa-regular fa-circle-stop"></i>&nbsp;${prop.TO_NAME}
+            `);
+            //console.log(feature.properties);
+        }
+    }).addTo(themaLayer.lines);
+}
+async function showSights(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    L.geoJSON(jsondata, {
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            layer.bindPopup(`
+            <img src="${prop.THUMBNAIL}" alt="*">
+            <h4><a href="${prop.WEITERE_INF}" target="Wien">${prop.NAME}</a></h4> 
+            <adress>${prop.ADRESSE}</adress>
+            `);
+            //console.log(feature.properties, prop.NAME);
+        }
+    }).addTo(themaLayer.sights);
+}
+async function showZones(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    L.geoJSON(jsondata, {
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            layer.bindPopup(`
+            <h4>Fußgängerzone&nbsp;${prop.ADRESSE}</h4>
+            <i class="fa-regular fa-clock"></i>&nbsp;${prop.ZEITRAUM}<br><br>
+            <i class="fa-solid fa-circle-info"></i>&nbsp;${prop.AUSN_TEXT}
+            `);
+            //console.log(feature.properties);
+        }
+    }).addTo(themaLayer.zones);
+}
+
+showStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+showLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+showZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+showSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
