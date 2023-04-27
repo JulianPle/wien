b93210d816cbcca4279274f402ae8c7619ced7a4
JulianPle <julian.plepla@gmx.de>
2023-04-27 16:58:43 +0200

    fullscreen plugin

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ let stephansdom = {
 let map = L.map("map").setView([
     stephansdom.lat, stephansdom.lng
 ], 15);
+map.addControl(new L.Control.Fullscreen ())
+
 //thematische Layer
 let themaLayer = {
     stops: L.featureGroup(),
-    lines: L.featureGroup(). addTo(map),
+    lines: L.featureGroup(),
     zones: L.featureGroup(),
-    sights: L.featureGroup()
+    sights: L.featureGroup(). addTo(map),
 }
 
 // Hintergrundlayer
